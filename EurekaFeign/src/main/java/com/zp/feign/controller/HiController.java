6ab80418a569df251285a80dce6cbd857081feaf
z <z>
2019-05-07 15:29:51 +0800

    更新

--- EurekaFeign/src/main/java/com/zp/feign/controller/HiController.java
+++ EurekaFeign/src/main/java/com/zp/feign/controller/HiController.java
@@ -2,6 +2,7 @@ package com.zp.feign.controller;
 
 import com.zp.feign.config.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,9 +10,12 @@ public class HiController {
 
     @Autowired
     SchedualServiceHi schedualServiceHi;
+    @Value("${spring.application.name}")
+    String appName;
+
     @GetMapping("/hi")
     public String sayHi(@RequestParam String name){
-        return schedualServiceHi.sayHiFromClientOne(name);
+        return schedualServiceHi.sayHiFromClientOne(name+appName);
     }
 
     @GetMapping("/")
--- ribbon/src/main/java/com/zp/ribbon/controller/HelloControler.java
+++ ribbon/src/main/java/com/zp/ribbon/controller/HelloControler.java
@@ -2,6 +2,7 @@ package com.zp.ribbon.controller;
 
 import com.zp.ribbon.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,10 @@ public class HelloControler {
 
     @Autowired
     HelloService helloService;
-
+    @Value("${spring.application.name}")
+    String appName;
     @GetMapping(value = "/hi")
     public String hi(@RequestParam String name) {
-        return helloService.hiService( name );
+        return helloService.hiService( name+appName );
     }
 }
