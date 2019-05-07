dc2c9736c39193d60169a65f776b185567c7ab81
z <z>
2019-05-07 10:21:39 +0800

    更新

--- EurekaFeign/src/main/java/com/zp/feign/config/SchedualServiceHi.java
+++ EurekaFeign/src/main/java/com/zp/feign/config/SchedualServiceHi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "service-client",fallback = SchedualServiceHiService.class)
-public interface    SchedualServiceHi {
+public interface SchedualServiceHi {
     @RequestMapping(value = "",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
--- EurekaFeign/src/main/java/com/zp/feign/controller/HiController.java
+++ EurekaFeign/src/main/java/com/zp/feign/controller/HiController.java
@@ -2,22 +2,19 @@ package com.zp.feign.controller;
 
 import com.zp.feign.config.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HiController {
 
     @Autowired
     SchedualServiceHi schedualServiceHi;
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    @GetMapping("/hi")
     public String sayHi(@RequestParam String name){
         return schedualServiceHi.sayHiFromClientOne(name);
     }
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @GetMapping("/")
     public String index(){
         return "hello";
     }
