b0ae3322153a210cce1256ce2de26c1eb7ba6641
zp <954480192@qq.com>
2019-09-17 17:29:04 +0800

    security测试

---
+++ user/src/main/java/com/example/user/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("loginPage")
+    public String login(){
+        return "login";
+    }
+}
