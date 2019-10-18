218db2d34e8d28f5ff6afc1eb80f7dabaebcddfd
zp <954480192@qq.com>
2019-10-18 17:14:28 +0800

    sleuth

--- nacos-comsumer/src/main/java/com/zp/nacoscomsumer/demo/ForeignConfiguration.java
+++ nacos-comsumer/src/main/java/com/zp/nacoscomsumer/demo/ForeignConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RestController
 public class ForeignConfiguration {
 
     @Autowired
