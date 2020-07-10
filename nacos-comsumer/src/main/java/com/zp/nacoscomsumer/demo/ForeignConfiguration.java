ad1237124538a4a1cec9a52f42729debbc48e31e
zp <954480192@qq.com>
2020-07-10 16:34:05 +0800

    LoadBalancerClient

--- nacos-comsumer/src/main/java/com/zp/nacoscomsumer/demo/ForeignConfiguration.java
+++ nacos-comsumer/src/main/java/com/zp/nacoscomsumer/demo/ForeignConfiguration.java
@@ -3,7 +3,9 @@ package com.zp.nacoscomsumer.demo;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.zp.nacoscomsumer.ForeignClient.RemoteClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class ForeignConfiguration {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     // -----------------  ribbon ----------------------
     @Bean
@@ -27,6 +31,11 @@ public class ForeignConfiguration {
     @GetMapping("/consumer")
     @SentinelResource(value="consumer")
     public String test1() {
+        //使用 LoadBalanceClient 和 RestTemplate 结合的方式来访问
+//        ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-provider");
+//        String url = String.format("http://%s:%s/%s",serviceInstance.getHost(),serviceInstance.getPort(),"helloNacos");
+//        System.out.println("request url:"+url);
+//        return restTemplate.getForObject(url,String.class);
         return restTemplate.getForObject("http://nacos-provide/helloNacos",String.class);
     }
 
